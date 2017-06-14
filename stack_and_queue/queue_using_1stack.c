44413be8f1ce46dbb05171a3c167beb5dc92b934
nileshchordia <nileshchordia93@gmail.com>
2017-06-14 23:57:38 +0530

    implemented in c

--- stack_and_queue/queue_using_1stack.c
+++ stack_and_queue/queue_using_1stack.c
@@ -1,11 +1,55 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define MAX 100
+
+int stack[MAX];
+int top=-1;
+
+
+void push(int data)
+{
+	stack[++top]=data;
+}
+
+int pop()
+{
+	int item=stack[top--];
+	return item;
+}
+
+
+void reverse()
+{
+	if(top>0)
+	{
+		int x=pop();
+		reverse();
+		push(x);
+	}
+	else
+		printf("%d\n",stack[top--] );
+
+}
+int dequeue()
+{
+	reverse();
+}
+void enqueue(int data)
+{
+	push(data);
+}
+
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	enqueue(1);
+	enqueue(2);
+	enqueue(3);
+	dequeue();
+	dequeue();
+	enqueue(4);
+	dequeue();
+	dequeue();
 	return 0;
 }
 
-//time complexity -----------orderof(n)
-//space complexity---------------orderof(n)
