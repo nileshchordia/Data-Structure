6e9953642ba984aadda1745745594d2adcb2a5c8
nileshchordia <nileshchordia93@gmail.com>
2017-06-15 00:17:23 +0530

    prefix_notes

---
+++ tree/prefix_infix_notes.c
@@ -0,0 +1,20 @@
+// infix to postfix NOTES-------------------
+
+
+// please watch before Interview , on preparation and on revision.
+
+
+
+// 	infix		|   prefix		|  postfix
+// ------------------------------------------------|
+// 	a+b			|	+abc    	|	ab+			|
+
+// 	(a+b)*c		|	*+abc   	|	ab+c*		|
+
+// 	a*(b+c)		|	*a+bc    	|	abc+*		|
+
+// 	a/b+c/d		|	+/ab/cd   	|	ab/cd/+		|
+												
+// 	(a+b)*(c+d)	|	*+ab+cd   	|	ab+cd+*		|
+
+// 	((a+b)*c)-d	|	-*+abcd   	|	ab+c*d-		|
